0c644ee8b5f89f68a3298781a4dc42e74678788e
Christian Bound <automaticbound@gmail.com>
2022-08-23 16:23:02 +0900

    songs added player controls done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [songs, setSongs] = useState([
+    {
+      "title": "Ahh Oh",
+      "artist": "Adult Time",
+      "album": "Joy Imported From A Happier Age",
+      "track": "1",
+      "year": "2022",
+      "img_src": "./songs_images/cover.jpg",
+      "src": "./songs/Ahh Oh.wav",
+    },
+    {
+      "title": "Shrine Lickers",
+      "artist": "Adult Time",
+      "album": "Joy Imported From A Happier Age",
+      "track": "2",
+      "year": "2022",
+      "img_src": "./songs_images/cover.jpg",
+      "src": "./songs/Shrine Lickers2.wav",
+    },
+    {
+      "title": "Things Keep Happening",
+      "artist": "Adult Time",
+      "album": "Joy Imported From A Happier Age",
+      "track": "3",
+      "year": "2022",
+      "img_src": "./songs_images/cover.jpg",
+      "src": "./songs/Things Keep Happening2.wav",
+    },
+    {
+      "title": "New Lighthouses",
+      "artist": "Adult Time",
+      "album": "Joy Imported From A Happier Age",
+      "track": "4",
+      "year": "2022",
+      "img_src": "./songs_images/cover.jpg",
+      "src": "./songs/New Lighthouses2.wav",
+    },
+    {
+      "title": "New Normal",
+      "artist": "Adult Time",
+      "album": "Joy Imported From A Happier Age",
+      "track": "5",
+      "year": "2022",
+      "img_src": "./songs_images/cover.jpg",
+      "src": "./songs/New Normal2.wav",
+    },
+  ]);
+
 }
 
 export default App;
